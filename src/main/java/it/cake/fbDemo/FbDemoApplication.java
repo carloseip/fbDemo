778c14def30615316b9e56b574fb30296254e7af
Carlos Inga Principe <carlos_eduprincipe@outlook.com>
2018-11-25 03:43:08 -0500

    Añadir repositorio a GitHub

---
+++ src/main/java/it/cake/fbDemo/FbDemoApplication.java
@@ -0,0 +1,12 @@
+package it.cake.fbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbDemoApplication.class, args);
+	}
+}
